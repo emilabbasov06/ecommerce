c2e60d665b5041a6308dc3522ffe3d1a2f8c68ca
emilabbasov06 <emilabb13@gmail.com>
2025-01-19 19:14:47 +0400

    working on crud

--- api/main.py
+++ api/main.py
@@ -1,6 +1,20 @@
 import pymysql
 import pymysql.cursors
 from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from typing import Optional
+
+# Category
+class Category(BaseModel):
+  category_name: str
+
+# Product
+class Product(BaseModel):
+  product_name: str
+  content: str
+  price: float
+  date_added: str
+  category: Optional[Category]
 
 
 # Connect to Database
@@ -15,6 +29,16 @@ DB = pymysql.connect(
 
 # Create cursor to execute queries
 cursor = DB.cursor()
+
+
+def category(category_name):
+  cursor.execute(f"SELECT * FROM categories WHERE category_name='{category_name}'")
+  identified_category = cursor.fetchone()
+  if identified_category:
+    return identified_category.get('category_id')
+  
+  return -1
+
 
 # Create the main pin-point for API
 app = FastAPI()
@@ -37,6 +61,21 @@ def get_product(id: int):
   return {'product': product}
 
 
+@app.post('/products', status_code=status.HTTP_201_CREATED)
+def create_product(product: Product):
+  CATEGORY_NAME = product.category.category_name
+  CATEGORY_ID = category(CATEGORY_NAME)
+  
+  if CATEGORY_ID != -1:
+    cursor.execute("""INSERT INTO products (product_name, content, price, date_added, category)
+    VALUES (%s, %s, %s, %s, %s)""", (product.product_name, product.content, product.price, product.date_added, CATEGORY_ID))
+    DB.commit()
+    
+    return {'message': 'New product added succesfully!'}
+  
+  return {'message': 'There was some technical problem'}
+  
+
 @app.get('/categories')
 def get_categories():
   cursor.execute('SELECT * FROM categories')
@@ -52,4 +91,11 @@ def get_product(id: int):
   if not category:
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Category with {id} was not found!")
   
-  return {'product': category}
+  return {'category': category}
+
+
+@app.post('/categories', status_code=status.HTTP_201_CREATED)
+def create_category(category: Category):
+  cursor.execute("""INSERT INTO categories (category_name) VALUES(%s)""", (category.category_name))
+  DB.commit()
+  return {'message': 'New Category added to Database'}
